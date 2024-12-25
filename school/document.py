ae992052373b7600c291246d59f3af19723a5332
Jaroslav T <jarileslava@gmail.com>
2024-12-25 17:25:52 +0100

    fix 3

--- school/document.py
+++ school/document.py
@@ -3,63 +3,72 @@ from datetime import datetime
 
 from faker import Faker
 
+
 class Document:
-    TASK_TEMPLATE = "Task-###"
-    MAXIMUM_GRADE = 5
-    MINIMUM_GRADE = 1
-    JOURNAL_PLACE = "./journal.json"
-    DATA_FORMAT = "%d.%m.%Y"
     faker = Faker()
 
-    def __init__(self):
-        pass
+    def __init__(
+            self,
+            task_template: str,
+            minimum_grade: int,
+            maximum_grade: int,
+            journal_place: str,
+            data_format: str,
+    ):
+        self.task_template = task_template
+        self.minimum_grade = minimum_grade
+        self.maximum_grade = maximum_grade
+        self.journal_place = journal_place
+        self.data_format = data_format
+
+    def validate_created_journal(self):
+        try:
+            with open(self.journal_place, "r") as file:
+                journal_file = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            journal_file = {"data": []}
+
+        return journal_file
 
     def get_number_task(self):
         """Генерирует рандомный номер таски формата TASK_TEMPLATE"""
 
-        task_number = self.faker.bothify(text=self.TASK_TEMPLATE)
+        task_number = Document.faker.bothify(text=self.task_template)
         return task_number
 
     def get_now_date(self):
         """Получить текущую дату"""
 
-        now_date = datetime.now().strftime(self.DATA_FORMAT)
+        now_date = datetime.now().strftime(self.data_format)
         return now_date
-
 
     def input_grade_main_journal(self, grade, student_data, task_id):
         """Добавляет в общий журнал оценку студента за выполненную задачу"""
 
-        try:
-
-            with open(self.JOURNAL_PLACE, "r") as file:
-                journal_file = json.load(file)
-        except (FileNotFoundError, json.JSONDecodeError):
-            journal_file = {"data":[]}
-
+        journal_file = self.validate_created_journal()
         now_date = self.get_now_date()
 
-
         new_dct = {
-            "task":task_id,
+            "task": task_id,
             "student": str(student_data),
             "create_at": now_date,
-            "score": grade
+            "score": grade,
         }
         journal_file["data"].append(new_dct)
 
-        with open(self.JOURNAL_PLACE, "w") as file:
+        with open(self.journal_place, "w") as file:
             json.dump(journal_file, file, indent=2)
 
             print(f"Оценка {grade} поставлена студенту {student_data}")
 
-    def input_grade_personal_journal(self, grade, task_id, personal_journal):
-        """Добавляет в дневник студента оценку за выполненную задачу"""
+    def get_student_grades_journal(self, student_data):
+        student_grades = {student_data: []}
+        journal_file = self.validate_created_journal()
 
-        now_date = self.get_now_date()
-        grade_str_personal = f"{task_id}  Date: {now_date}"
+        for data in journal_file["data"]:
 
-        personal_journal[grade_str_personal] = grade
-        return personal_journal
+            if data["student"] == student_data:
+                format_data = {"task": data["task"], "create_at": data["create_at"]}
+                student_grades[student_data].append(format_data)
 
-
+        return student_grades
